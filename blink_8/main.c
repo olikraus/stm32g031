843f1f73014122b30114cdbe03f67241dd1cbc57
kraus <olikraus@gmail.com>
2021-11-13 16:25:09 +0100

    enable boot0 code

---
+++ blink_8/main.c
@@ -0,0 +1,55 @@
+/* 
+  LED blink project for the STM32G031
+  
+  Default clock is HSI16 (16 MHz)
+  
+  
+  Ensure that -DUSER_VECT_TAB_ADDRESS is set during compilation, otherwise
+  interrupts will not work after the "go" commant of the flashware USART upload.
+  
+  Problem is, that SCB->VTOR doesn't point to FLASH_BASE, so manually
+  assigning SCB->VTOR = FLASH_BASE;	will also fix this.
+
+  If -DUSER_VECT_TAB_ADDRESS is defined, then the SCB->VTOR is set in
+  SystemInit(void) which is called by the reset handler (.s file).
+
+*/
+
+#include "stm32g0xx.h"
+
+volatile unsigned long SysTickCount = 0;
+
+void __attribute__ ((interrupt, used)) SysTick_Handler(void)
+{
+  SysTickCount++;  
+  
+  if ( SysTickCount & 1 )
+    GPIOA->BSRR = GPIO_BSRR_BS3;		/* atomic set PA13 */
+  else
+    GPIOA->BSRR = GPIO_BSRR_BR3;		/* atomic clr PA13 */
+}
+
+int main()
+{
+  RCC->IOPENR |= RCC_IOPENR_GPIOAEN;		/* Enable clock for GPIO Port A */
+  __NOP();
+  __NOP();
+
+  GPIOA->AFR[0] &= ~(0xf << (3*4));       /* clear alternative function */
+  //GPIOA->AFR[0] = 0;       /* clear alternative function */
+  
+  GPIOA->MODER &= ~GPIO_MODER_MODE3;	/* clear mode for PA3 */
+  GPIOA->MODER |= GPIO_MODER_MODE3_0;	/* Output mode for PA3 */
+  GPIOA->OTYPER &= ~GPIO_OTYPER_OT3;	/* no Push/Pull for PA3 */
+  GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED3;	/* low speed for PA3 */
+  GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD3;	/* no pullup/pulldown for PA3 */
+  GPIOA->BSRR = GPIO_BSRR_BR3;		/* atomic clr PA3 */
+  
+
+  SysTick->LOAD = 16000*500 - 1;        // Blink with 1 Hz 
+  SysTick->VAL = 0;
+  SysTick->CTRL = 7;   /* enable, generate interrupt (SysTick_Handler), do not divide by 2 */
+    
+  for(;;)
+    ;
+}
