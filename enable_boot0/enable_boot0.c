f03d166a399ea3717ca4606eb98634476f437518
kraus <olikraus@gmail.com>
2021-11-13 17:04:23 +0100

    update

--- enable_boot0/enable_boot0.c
+++ enable_boot0/enable_boot0.c
@@ -2,6 +2,38 @@
   
   enable_boot0.c
   
+  
+  BSD 3-Clause License
+
+  Copyright (c) 2021, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+
+
   Background:
     The BOOT0 pin is disabled for STM32G0 devices by default: The bootloader
     will not be executed with a high level applied to BOOT0 pin during reset.
@@ -29,8 +61,7 @@
 
 int main()
 {
-  /* check for the BOOT0 selection to avoid reflashing */
-  
+  /* check for the BOOT0 selection to avoid reflashing */  
   if ( (FLASH->OPTR & FLASH_OPTR_nBOOT_SEL) == 0 )
   {
     /* nBOOT_SEL is already cleared..., do nothing */
